6a3e732bdd36d1f67f24d296d167fb7cc302383c
HyHy1506 <tran1106duc@gmail.com>
2024-11-24 19:12:56 +0700

    edit start activity

--- app/src/main/java/com/example/worldstory/StartActivity.kt
+++ app/src/main/java/com/example/worldstory/StartActivity.kt
@@ -6,16 +6,30 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityStartBinding
+import android.os.Looper
+import android.os.Handler
+import android.window.SplashScreen
+import com.example.worldstory.duc.ducactivity.DucUserHomeActivity
+import com.example.worldstory.duc.ducutils.toActivity
 
 class StartActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityStartBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_start)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding= ActivityStartBinding.inflate(layoutInflater)
+        val view =binding.root
+        setContentView(view)
+
+        val handler= Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            toActivity(DucUserHomeActivity::class.java)
+        },2000)
+
+
     }
 }
